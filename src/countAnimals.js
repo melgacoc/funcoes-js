0d6460ff92461b34e04497917fc3cda8b6cbe572
Claudio Melgaco <claudio19a@hotmail.com>
2022-07-10 20:52:19 -0300

    req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    return data.species.find((element) => element.name === animal.specie)
+      .residents.length;
+  }
+  return data.species.find((element) => element.name === animal.specie)
+    .residents.filter((element) => element.sex === animal.sex).length;
 }
+
 
 module.exports = countAnimals;
