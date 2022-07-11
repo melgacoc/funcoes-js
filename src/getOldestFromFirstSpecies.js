49012a8482856e24c69e0919d7b8122df22ac593
Claudio Melgaco <claudio19a@hotmail.com>
2022-07-10 21:16:50 -0300

    req 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = employees.find((element) => element.id === id).responsibleFor[0];
+  const animal = species.find((element) => element.id === employee).residents;
+  const age = animal.sort((a, b) => b.age - a.age)[0];
+  return [age.name , age.sex, age.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
